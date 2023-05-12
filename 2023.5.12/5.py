4a0889f5a7612c16792e38cc7c46799338c53b3e
Zhangfeng <zjy20040723@163.com>
2023-05-12 11:41:35 +0800

    update

--- 2023.5.12/5.py
+++ 2023.5.12/5.py
@@ -1,15 +1,12 @@
 def fun(num):
-    i = 0
+    list = []
     while num > 0:
         digit = num % 10
+        list.append(digit)
         num = num // 10
-        i = i + 1
-
-    n = 0
-    while num > 0:
-        digit = num % 10
-        list[n] = digit
-        num = num // 10
-        n = n + 1
+    sum = 0
+    for i in range(0, len(list)):
+        sum = sum + list[i]
+    print(sum)
 
 fun(123)
